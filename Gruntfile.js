b7b2347c38c1dbc7234247984ffef71ff4a971ef
jonnydark <jonny@jonnydark.com>
2014-07-31 22:36:27 +0100

    Added run and jshint tasks to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
 module.exports = function(grunt) {
+    var dev_target     = 'fps-engine-dev.js';
+    var target         = 'fps-engine.js';
+    var target_browser = 'google-chrome';
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -6,20 +9,32 @@ module.exports = function(grunt) {
         concat: {
             build: {
                 src: ['src/*.js'],
-                dest: 'fps-engine-dev.js'
+                dest: dev_target
             }
         },
 
         uglify: {
             my_target: {
                 files: {
-                    'fps-engine.js' : ['fps-engine-dev.js']
+                    target : [dev_target]
                 }
             }
         },
 
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
+            built: [dev_target]
+        },
+
         qunit : { 
             all : ['test/**/*.html']
+        },
+
+        exec : {
+           run  :        target_browser + ' development.html',
+           run_release : target_browser + ' production.html'
         }
 
     });
@@ -27,10 +42,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-exec');
 
 
-    grunt.registerTask('default', ['concat']);
-    grunt.registerTask('release', ['concat', 'uglify']);
+    grunt.registerTask('default', ['concat', 'jshint']);
+    grunt.registerTask('run', ['exec:run']);
+    grunt.registerTask('run_release', ['exec:run_release']);
+    grunt.registerTask('release', ['concat', 'jshint', 'uglify']);
     grunt.registerTask('test', ['qunit']);
 
 };
