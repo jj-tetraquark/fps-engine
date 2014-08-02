d5322c4c4612432c07f772ae279f7f76233d246a
jonnydark <jonny@jonnydark.com>
2014-08-02 17:13:27 +0100

    DebugConsole constructor defined

--- src/DebugConsole.js
+++ src/DebugConsole.js
@@ -1,3 +1,6 @@
 function DebugConsole() {
-
+    // create the window
+    var debugConsole = document.createElement('div');
+    debugConsole.id = 'debug-console';
+    document.body.appendChild(debugConsole);
 }
--- test/DebugConsoleTests.js
+++ test/DebugConsoleTests.js
@@ -1,4 +1,7 @@
 QUnit.test("Test construction of Debug Console", function(assert) {
     dbg = new DebugConsole();
     assert.ok(dbg);
+    assert.ok(document.getElementById('debug-console'), "Construction of debug console should " +
+        "make div called debug-console. Nothing found with that ID");
 });
+
