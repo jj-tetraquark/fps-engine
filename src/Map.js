1fe01cbbcfb921eab745b1731d81401c0de1486d
jonnydark <jonny@jonnydark.com>
2014-08-03 00:26:28 +0100

    Extended map constructor and defined wall grid

--- src/Map.js
+++ src/Map.js
@@ -1,3 +1,11 @@
-function Map(size) {
-
+function Map(lengthOrJson, width) {
+    if (typeof(lengthOrJson) === "number") {
+        if (width === undefined) {
+            this._wallGrid = new Uint8Array(lengthOrJson * lengthOrJson);
+        } else {
+            this._wallGrid = new Uint8Array(lengthOrJson * width);
+        }
+    } else {
+        this._wallGrid = new Uint8Array(lengthOrJson[0].length * lengthOrJson.length);
+    }
 }
--- test/MapTests.js
+++ test/MapTests.js
@@ -1,4 +1,21 @@
 QUnit.test("Test construction of Map object", function(assert) {
     var testMap = new Map(10);
     assert.ok(testMap);
+    assert.equal(testMap._wallGrid.length, 100);
+
+    var testMap2 = new Map(10,20);
+    assert.ok(testMap2);
+    assert.equal(testMap2._wallGrid.length, 200);
+
+    var testMap3 = new Map([
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0]
+                            ]);
+
+    assert.ok(testMap3);
+    assert.equal(testMap3._wallGrid.length, 40);
+
+
 });
