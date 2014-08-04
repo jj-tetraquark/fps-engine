1f3431b3a386b1ee621279466990f0fa9e84f932
Jonathan Wilkinson <jonathan.wilkinson@dyson.com>
2014-08-04 16:10:38 +0100

    Added function to load an external JSON file

---
+++ src/loadJSON.js
@@ -0,0 +1,13 @@
+// Function to load an external json file
+function loadJSON(path, callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true); 
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);  
+}
